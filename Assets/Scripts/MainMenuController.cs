a6bd4e12b9f8f1903f2477f88ac975305a2ea039
Chiragatrey70 <chaggyatrey2005@gmail.com>
2025-09-30 08:56:19 +0530

    .

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -3,15 +3,48 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    public void PlayGame()
+    [Header("UI Panels")]
+    public GameObject mainMenuPanel;
+    public GameObject objectivePanel;
+
+    void Start()
     {
-        // Make sure your game scene is named "SampleScene"
+        // Ensure the correct panels are visible at the start
+        mainMenuPanel.SetActive(true);
+        objectivePanel.SetActive(false);
+    }
+
+    // NEW: This function runs every frame
+    private void Update()
+    {
+        // First, check if the objective panel is currently visible
+        if (objectivePanel.activeInHierarchy)
+        {
+            // Then, check if the player presses the "Enter" key (Return) or the Numpad Enter key
+            if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+            {
+                // If they do, call the same function that the "Continue" button uses
+                StartGame();
+            }
+        }
+    }
+
+    public void ShowObjectiveScreen()
+    {
+        // This function is called by the "Play" button
+        mainMenuPanel.SetActive(false);
+        objectivePanel.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        // This function is called by the "Continue" button and now by the Enter key
         SceneManager.LoadScene("SampleScene");
     }
 
     public void ExitGame()
     {
-        Debug.Log("Exiting game..."); // This message shows in the editor
+        Debug.Log("Exiting game...");
         Application.Quit();
     }
 }
